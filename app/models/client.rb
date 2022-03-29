f065ca5e855f6a3be7bfc28378f702c7728bd30e
Denis <icebox827@gmail.com>
2022-03-29 02:30:58 -0400

    Create associations and validations for all models

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,17 @@
 class Client < ApplicationRecord
+  # Validations
+  validates :first_name, presence: true, length: { minimum: 2, maximum: 20 }
+  validates :last_name, presence: true, length: { minimum: 2, maximum: 25 }
+  validates :email, presence:true, uniqueness: { case_sensitive: true }
+  validates_format_of :email, with: /\A\S+@.+\.\S+\z/
+  validates :company_id, presence:true
+
+  # Associations
+  belongs_to :company
+  has_many :addresses, dependent: :destroy
+
+  # Built-in method to count clients
+  def self.client_count
+    @number_client = Client.group(:company_id).count
+  end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,7 @@
 class Company < ApplicationRecord
+  # Validations
+  validates :name, presence :true, length: { minimum:2, maximum:30 }
+
+  # Associations
+  has_many :clients, dependent: :destroy
 end
