6fb171cda48d079deed96e784e061fa4cbcc2d5f
Denis <icebox827@gmail.com>
2022-03-29 09:48:30 -0400

    Create company controller

---
+++ app/controllers/api/v1/companies_controller.rb
@@ -0,0 +1,39 @@
+module Api
+  module V1 
+    class CompaniesController < ApplicationController
+      # Display company
+      def show
+        @company = Company.find(params[:id])
+        render json: @company
+      end
+
+      # Create company
+      def create
+        @company = Company.new(company_params)
+
+        if @company.save
+          render json: { messages: 'Created successfully' }, status: 200
+        else
+          render json: { error: @company.errors.full_messages }, status: 422
+        end
+      end
+
+      # Update company
+      def update
+        @company = Company.find(params[:id])
+
+        if @company.update(company_params)
+          render json: @company
+        else
+          render json: { error: @company.errors.full_messages }, status: 422
+        end
+      end
+
+      private
+      # Set company parameters
+      def company_params
+        params.require(:name)
+      end
+    end
+  end
+end
